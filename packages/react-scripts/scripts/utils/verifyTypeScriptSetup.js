f1eae5f41db019d4a6a5240580bddf8dcf26262c
Qolzam <amir.gholzam@live.com>
2019-05-04 14:10:13 +0700

    Add isolatedModules:true until changing all index files.

--- packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
+++ packages/react-scripts/scripts/utils/verifyTypeScriptSetup.js
@@ -114,7 +114,7 @@ function verifyTypeScriptSetup() {
       reason: 'to match webpack resolution',
     },
     resolveJsonModule: { value: true, reason: 'to match webpack loader' },
-    isolatedModules: { value: true, reason: 'implementation limitation' },
+    isolatedModules: { value: false, reason: 'implementation limitation' },
     noEmit: { value: true },
     jsx: {
       parsedValue: ts.JsxEmit.Preserve,
